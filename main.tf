b3d5c1718bff693d8602682301c3d0a5cc886cc7
Alberto Sabaini <alberto.sabaini@univr.it>
2020-07-10 14:49:31 +0200

    remove provider from main.tf

--- main.tf
+++ main.tf
@@ -2,13 +2,6 @@ terraform {
   required_version = ">= 0.12, < 0.13"
 }
 
-provider "aws" {
-  region = "eu-central-1"
-
-  # Allow any 2.x version of the AWS provider
-  version = "~> 2.0"
-}
-
 module "bucket-home-folders" {
   source             = "./modules/data-stores/bucket-home-folders"
   bucket_name        = var.bucket_name
