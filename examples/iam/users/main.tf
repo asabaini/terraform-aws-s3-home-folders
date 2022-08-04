3ab4eda6040a7aba85e699de954fe80091d5e44f
Alberto Sabaini <sabainialberto@gmail.com>
2022-08-04 11:15:41 +0300

    switch to aws_s3_object

--- examples/iam/users/main.tf
+++ examples/iam/users/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      version = "~> 2.0"
+      version = ">= 3.63"
       source  = "hashicorp/aws"
     }
   }
--- modules/data-stores/bucket-home-folders/main.tf
+++ modules/data-stores/bucket-home-folders/main.tf
@@ -2,7 +2,7 @@ data "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
 }
 
-resource "aws_s3_bucket_object" "home_folders" {
+resource "aws_s3_object" "home_folders" {
   for_each = { for k, v in var.home_folder_names : k => v if var.create_personal_folders }
 
   bucket = data.aws_s3_bucket.bucket.id
@@ -10,7 +10,7 @@ resource "aws_s3_bucket_object" "home_folders" {
   source = "/dev/null"
 }
 
-resource "aws_s3_bucket_object" "shared_folder" {
+resource "aws_s3_object" "shared_folder" {
   bucket = data.aws_s3_bucket.bucket.id
   key    = format("%s/", var.shared_folder_name)
   source = "/dev/null"
